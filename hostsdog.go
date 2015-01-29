a5969ad83d9339fb5cd77af0085321ab9fc063da
haosdent <haosdent@gmail.com>
2015-01-29 23:48:01 +0800

    Update Readme

---
+++ hostsdog.go
@@ -0,0 +1,23 @@
+pacakge hostsdog
+
+import (
+)
+
+func initDir() {
+
+}
+
+func generateHosts(config string) {
+}
+
+func AddHosts(config string) {
+}
+
+func RmHosts(config string) {
+}
+
+func ForkHosts(oldConfig string, newConfig string) {
+}
+
+func UseHosts(config string) {
+}
