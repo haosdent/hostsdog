ff9560ff46f8ccde7e0403ad020b2e564b6b672f
haosdent <haosdent@gmail.com>
2015-02-07 00:46:31 +0800

    Update main.go

--- src/main.go
+++ src/main.go
@@ -2,8 +2,20 @@ package main
 
 import (
     "os"
+    "fmt"
     "hostsdog"
 )
+
+func usage() {
+    fmt.Printf("hello\n")
+}
+
+func checkArgs(requireSize int) {
+    if len(os.Args) != requireSize {
+        usage()
+        os.Exit(-1)
+    }
+}
 
 func main() {
     dog := hostsdog.NewHostsdog("/tmp/hostsdog", "/tmp/hosts")
@@ -12,9 +24,27 @@ func main() {
         return
     }
 
-    //dog.AddHosts("base")
-    //dog.RmHosts("base")
-    //dog.ForkHosts("base", "new")
-    //dog.SwitchHosts("new")
-    //dog.ListHosts()
+    switch act := os.Args[1]; act {
+    case "add":
+        checkArgs(3)
+        config := os.Args[2]
+        dog.AddHosts(config)
+    case "rm":
+        checkArgs(3)
+        config := os.Args[2]
+        dog.RmHosts(config)
+    case "fork":
+        checkArgs(4)
+        oldConfig := os.Args[2]
+        newConfig := os.Args[3]
+        dog.ForkHosts(oldConfig, newConfig)
+    case "switch":
+        checkArgs(3)
+        config := os.Args[2]
+        dog.SwitchHosts(config)
+    case "list":
+        dog.ListHosts()
+    default:
+        usage()
+    }
 }
